ef1b8df995ff05fc440152e260c8d16d6691d21d
DanielNetzer <daniel.netzer7@gmail.com>
2018-12-23 13:46:06 +0200

    feat(updated release process): updated release process

--- packages/libA/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("@golda/libb/src");
-new src_1.Hello("daniel");
-//# sourceMappingURL=index.js.map
--- packages/libB/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class Hello {
-  constructor(helloWho?: string);
-}
